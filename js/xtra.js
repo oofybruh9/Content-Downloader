f945bd4e2dc253e28a31c4bc6f0821510bea5a81
oofybruh9 <122695223+oofybruh9@users.noreply.github.com>
2024-10-05 03:12:45 +0000

    new file:   img/logo/Archivador.zip 	new file:   img/logo/dolphin.png 	new file:   img/logo/gbep.png 	new file:   img/logo/kega.gif 	new file:   img/logo/mame.jpg 	new file:   img/logo/mednafen.png 	new file:   img/logo/mgba.png 	new file:   img/logo/picodrive.gif 	new file:   img/logo/ppsspp.jpg 	new file:   img/logo/retroarch.jpg 	new file:   img/logo/snes9x.png 	new file:   img/logo/wave.webp 	modified:   index.html 	modified:   js/xtra.js

--- js/xtra.js
+++ js/xtra.js
@@ -1,1 +1,12 @@
-var li = document.querySelectorAll(".cool li")
+var li = document.querySelectorAll(".coolL li")
+for(var i = 0;i<li.length;i++){
+    li[i].addEventListener("click", myScript);
+}
+
+function myScript(e){
+    if (e.target.attributes.id.value == 1){
+        
+    }
+}
+console.log(li)
+alert(e.target.attributes.id.value);
