1b924274288d4fae4a8a7013a4b9d9da995a5a66
oofybruh9 <122695223+oofybruh9@users.noreply.github.com>
2024-10-07 01:53:11 +0000

    modified:   css/xtra.css 	deleted:    img/01.png 	deleted:    img/02.png 	deleted:    img/03.png 	deleted:    img/04.png 	deleted:    img/05.png 	deleted:    img/06.png 	renamed:    img/logo/dolphin.png -> img/dolphin.png 	renamed:    img/logo/gbep.png -> img/gbep.png 	renamed:    img/logo/huh.jpg -> img/huh.jpg 	renamed:    img/logo/kega.gif -> img/kega.gif 	renamed:    img/logo/mame.jpg -> img/mame.jpg 	renamed:    img/logo/mednafen.png -> img/mednafen.png 	renamed:    img/logo/mgba.png -> img/mgba.png 	renamed:    img/logo/picodrive.gif -> img/picodrive.gif 	renamed:    img/logo/ppsspp.jpg -> img/ppsspp.jpg 	renamed:    img/logo/retroarch.jpg -> img/retroarch.jpg 	renamed:    img/logo/snes9x.png -> img/snes9x.png 	renamed:    img/logo/wave.webp -> img/wave.webp 	modified:   index.html 	modified:   js/xtra.js

--- js/xtra.js
+++ js/xtra.js
@@ -6,12 +6,20 @@ for(var i = 0;i<li.length;i++){
 function myScript(e){
     if (e.target.attributes.id.value == 1){
         const image = document.getElementById('logo');
-        image.src = "../img/logo/retroarch.jpg"
+        image.src = "../img/retroarch.jpg"
         document.getElementById('um_actually').innerText = "RetroArch is a frontend for emulators, game engines and media players. Among other things, it enables you to run classic games on a wide range of computers and consoles through its slick graphical interface. Settings are also unified so configuration is done once and for all. In addition to this, you are able to run original game discs (CDs) from RetroArch. RetroArch has advanced features like shaders, netplay, rewinding, next-frame response times, runahead, machine translation, blind accessibility features, and more!";
     } else if (e.target.attributes.id.value == 2) {
         const image = document.getElementById('logo');
-        image.src = "../img/logo/snes9x.png";
+        image.src = "../img/snes9x.png";
         document.getElementById('um_actually').innerText = "Snes9x is a portable, freeware Super Nintendo Entertainment System (SNES) emulator. It basically allows you to play most games designed for the SNES and Super Famicom Nintendo game systems on your PC or Workstation; which includes some real gems that were only ever released in Japan.";
-        
+    } else if (e.target.attributes.id.value == 3) {
+        const image = document.getElementById('logo');
+        image.src = "../img/mame.jpg";
+        document.getElementById('um_actually').innerText = "MAME (formerly an acronym of Multiple Arcade Machine Emulator) is a free and open-source emulator designed to recreate the hardware of arcade games, video game consoles, old computers and other systems in software on modern personal computers and other platforms.";
+    } else if (e.target.attributes.id.value == 4)
+        const image = document.getElementById('logo');
+        const txt = document.getElementById('um_actually');
+        image.src = "../img/ppsspp.jpg"
+        txt.innerText = "PPSSPP (an acronym for PlayStation Portable Simulator Suitable for Playing Portably) is a multi-platform, open-source PlayStation Portable emulator written in C++ with the aim of speed and portability. Its development is ongoing and is still in a beta state, but progress is fast and most games are already playable. It is also available as a libretro core for RetroArch."
     }
 }
